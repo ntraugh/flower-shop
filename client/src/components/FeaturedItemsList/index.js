83dbeb93b718f4b363b729c77dad2088c130dc89
Nate Traugh <n.traugh@yahoo.com>
2022-08-02 13:12:23 -0400

    got featured items to populate on home page, need to get correct image, added useEffect to FeaturedItemsList

--- client/src/components/FeaturedItemsList/index.js
+++ client/src/components/FeaturedItemsList/index.js
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { QUERY_FEATURED } from "../../utils/queries"
 import { useQuery } from '@apollo/client';
 import FeaturedItems from '../FeaturedItems';
+import { idbPromise } from '../../utils/helpers';
+import { useStoreContext } from '../../utils/GlobalState';
 
 
 
 
 const FeaturedItemsList = () => {
+    const [state, dispatch] = useStoreContext();
+
     const { loading, data } = useQuery( QUERY_FEATURED )
+
+    useEffect(() => {
+        if (data) {
+          dispatch({
+            type: QUERY_FEATURED,
+            products: data.featured,
+          });
+          data.featured.forEach((product) => {
+            idbPromise('products', 'put', product);
+          });
+        } else if (!loading) {
+          idbPromise('products', 'get').then((products) => {
+            dispatch({
+              type: QUERY_FEATURED,
+              products: products,
+            });
+          });
+        }
+      }, [data, loading, dispatch]);
     return (
         <div>
             <h2>Featured Items:</h2>
-            {/* {data.map((product) => (
-                <FeaturedItems />
+                {data.featured.map((item) => (
+                    <FeaturedItems 
+                        key={item._id}
+                        _id={item._id}
+                        image={item.image}
+                        name={item.name}
+                        price={item.price}
+                        />
+                ))}
 
-            ))} */}
+            
         </div>
     )
 }
