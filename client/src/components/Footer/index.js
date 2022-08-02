35443d9ed7956f5a27f84d3cfb83422287afceab
Hans Reichgelt <hreichgelt06@gmail.com>
2022-08-02 11:36:32 -0400

    setting up footer need to utilize links

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer>
-      {/* Footer details HERE */}
+      <ul className='footerNav'>
+        <li className='hours'>
+          Monday-Friday: 8:00AM - 6:00PM
+          Saturday: 10:00AM - 8:00PM
+          Sunday: Noon-5:00PM
+        </li>
+        <li className='address'>
+          1 Rose Blvd 
+          Daisyville, NC 
+          </li>
+          <li className='linkToContact'>
+            <link></link>
+          </li>
+      </ul>
     </footer>
   );
 };
