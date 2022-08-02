d3e2fe074908eaf827243108ea2a948c12394e40
Hans Reichgelt <hreichgelt06@gmail.com>
2022-08-02 13:55:24 -0400

    contact and footer styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Footer.css'
 
 const Footer = () => {
   return (
     <footer>
       <ul className='footerNav'>
-        <li className='hours'>
-          Monday-Friday: 8:00AM - 6:00PM
-          Saturday: 10:00AM - 8:00PM
-          Sunday: Noon-5:00PM
+        <li className='listItem' id='hours'>
+          Monday-Friday: 8AM - 6PM
+          <br></br>
+          Saturday: 10AM - 8PM
+          <br></br>
+          Sunday: Noon-5PM
         </li>
-        <li className='address'>
+        <li className='listItem' id='address'>
           1 Rose Blvd 
           Daisyville, NC 
           </li>
-          <li className='linkToContact'>
-            <link></link>
+          <li className='listItem' id='contact'>
+            Contact Us
           </li>
       </ul>
     </footer>
