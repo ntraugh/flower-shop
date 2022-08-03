707ac0b561fd3ce7b00579e37c5eff3974cef361
grilledcheeseplease <lacynunnally402@gmail.com>
2022-08-02 23:30:29 -0400

    worked with styling and population bouquet and occasions

--- client/src/pages/Occasion.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Occasion() {
-  return (
-    <div>Occasion</div>
-  )
-}
-
-export default Occasion
---
+++ client/src/pages/Occasion/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./Occasion.css";
+
+function Occasion() {
+
+  return (
+    <main id='container'>
+      <div id='occH2'>
+        <h2>Occasions</h2>
+      </div>
+      <div id='occImgContainer'>
+        <img src='/images/anniversary.jpg' alt='Anniversary' id='occImage' />
+        <div id='imageOverlay'>
+          <div id='occTitle'><a href='/occasion/:id'>Anniversary</a></div>
+        </div>
+      </div>
+      <div id='occImgContainer'>
+        <img src='/images/birthday.jpg' alt='Birthday' id='occImage' />
+        <div id='imageOverlay'>
+          <div id='occTitle'><a href='/occasion/:id'>Birthday</a></div>
+        </div>
+      </div>
+      <div id='occImgContainer'>
+        <img src='/images/congrats.jpg' alt='Congratulations' id='occImage' />
+        <div id='imageOverlay'>
+          <div id='occTitle'><a href='/occasion/:id'>Congratulations</a></div>
+        </div>
+      </div>
+      <div id='occImgContainer'>
+        <img src='/images/getwell.jpg' alt='Get Well' id='occImage' />
+        <div id='imageOverlay'>
+          <div id='occTitle'><a href='/occasion/:id'>Get Well</a></div>
+        </div>
+      </div>
+      <div id='occImgContainer'>
+        <img src='/images/romance.jpg' alt='Romance' id='occImage' />
+        <div id='imageOverlay'>
+          <div id='occTitle'><a href='/occasion/:id'>Romance</a></div>
+        </div>
+      </div>
+      <div id='occImgContainer'>
+        <img src='/images/thanks.jpg' alt='Thank You' id='occImage' />
+        <div id='imageOverlay'>
+          <div id='occTitle'><a href='/occasion/:id'>Thank You</a></div>
+        </div>
+      </div>
+    </main>
+  )
+};
+
+export default Occasion;
