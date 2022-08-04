0175c3a6293e3e95308594b036d1243fa39ad711
Nate Traugh <n.traugh@yahoo.com>
2022-08-04 11:45:56 -0400

    created cart notification when items are added to cart

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Navbar from '../Navbar';
 import { Link } from "react-router-dom"
 import { FaShoppingCart } from "react-icons/fa"
+import { useStoreContext } from "../../utils/GlobalState";
 import "./HeaderStyle.css"
 
 const Header = (props) => {
+  const [state, dispatch] = useStoreContext();
+  const { cart } = state
+  let cartArray = cart
+  useEffect(() => {
+    const { cart } = state
+    let cartArray = cart
+  }, []);
+
   return (
     <>
    <div className='headerInfo'>
@@ -15,14 +24,15 @@ const Header = (props) => {
       </span>Flower Shop
       </Link>
     </h1>
+      <div className='cartIcon'>
         <Link to='/cart'>
           <FaShoppingCart
-                            
-          size={40}
-          onClick={() => props.setPage("Flower Shop | Checkout")}
-          style={"Flower Shop | Checkout" === props.page ? ({ color: "rgb(246, 189, 96)"}): null}
-          />
+            size={40}
+            onClick={() => props.setPage("Flower Shop | Checkout")}
+            style={"Flower Shop | Checkout" === props.page ? ({ color: "rgb(246, 189, 96)"}): null}
+            /> <p className='cartNoti'>{ cartArray.reduce((p, c) => p + c.purchaseQuantity, 0)  }</p>
         </Link>
+      </div>
     </div>
    <Navbar {...props} />
     </>
