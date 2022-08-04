0f5ef34ec6163a02ef2794c4406bad7d26f3dfcc
grilledcheeseplease <lacynunnally402@gmail.com>
2022-08-04 16:48:36 -0400

    .env and changes to bouquet page

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,8 @@
+require('dotenv');
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Bouquet, Occasion, Order } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+const stripe = require('stripe')(process.env.API);
 
 const resolvers = {
   Query: {
